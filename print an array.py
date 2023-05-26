d64c276ad38b034695964623b84d604bce54c07f
Kamal Nayan Yagik <105438015+Kamalkumar786@users.noreply.github.com>
2023-05-26 07:57:16 +0530

    Create print an array.py

---
+++ print an array.py
@@ -0,0 +1,11 @@
+def print_arr(arr,i,n):
+    if i<n:
+        print(arr[i],end=' ')
+        print_arr(arr,i+1,n)
+
+arr=eval(input("Enter an array:"))
+if arr==[]:
+    print("Empty Array!")
+else:
+    print("Printing an array:-")
+    print_arr(arr,0,len(arr))
